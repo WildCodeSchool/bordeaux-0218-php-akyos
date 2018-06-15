f34ee848c8d6324f0b4147c7093c20993818c552
Aurelien <aurelien@wildcodeschool.fr>
2018-06-15 14:24:00 +0200

    webpack init ok with LESS

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+require('../less/main.less');
+require('jquery');
+require('./core/bootstrap.min');
+require('./core/jquery.slimscroll.min');
+require('./core/jquery.scrollLock.min');
+require('./core/jquery.appear.min');
+require('./core/jquery.countTo.min');
+require('./core/jquery.placeholder.min');
+require('./core/js.cookie.min');
+require('./app.js');
+
+require('./plugins/slick/slick.js');
+require('./plugins/chartjs/Chart.min');
+//require('./pages/base_pages_dashboard');
