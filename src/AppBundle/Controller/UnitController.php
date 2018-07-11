6b65c5cbb890e735e960085ed66b9901e3a00c1d
gabriel2combe <gabriel.decombe@orange.fr>
2018-07-11 11:06:09 +0200

    Update UnitController.php

--- src/AppBundle/Controller/UnitController.php
+++ src/AppBundle/Controller/UnitController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Unit;
 use AppBundle\Entity\Condominium;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
